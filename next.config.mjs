4ed851cc82a315e558591fd97cc1958737e32eec
TiwariRahul16 <168175850+TiwariRahul16@users.noreply.github.com>
2024-11-24 23:54:12 +0530

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export', // Enable static exports
-    basePath: '/Web_Development', // Set the base path to your repository name
-    assetPrefix: '/Web_Development/', // Prefix for all assets
-};
-
-export default nextConfig;
+    output: 'export',
+    basePath: '/Web_Development',
+    assetPrefix: '/Web_Development/',
+  };
+  
+  export default nextConfig;
+  
